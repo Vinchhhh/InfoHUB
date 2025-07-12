549c6e08311bc6b70c8e26651941c4a40b3fb0de
Raphael Vinch Dulatre <145740610+Vinchhhh@users.noreply.github.com>
2025-07-12 22:26:21 +0800

    Add files via upload

---
+++ chatbot.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>InfoHUB</title>
+    <style>
+        df-messenger {
+            --df-messenger-bot-message: #df9b56;
+            --df-messenger-button-titlebar-color: #df9b56;
+            --df-messenger-chat-background-color: #fafafa;
+            --df-messenger-font-color: white;
+            --df-messenger-send-icon: #df9b56;
+            --df-messenger-user-message: #479b3d;
+        }
+    </style>
+</head>
+
+
+<body>
+    <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
+    <df-messenger
+        intent="WELCOME"
+        chat-title="InfoHUB"
+        agent-id="a15dcdc3-c200-47ab-9f3c-a872fe5cd5a0"
+        language-code="en">
+    </df-messenger>
+</body>
+
+</html>
